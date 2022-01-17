a386c1385edd4ae64c612597d3e205ca42afa510
Kevin <k.shabahang@gmail.com>
2022-01-17 08:01:09 -0500

    send it up

--- train.sh
+++ train.sh
@@ -1,10 +1,10 @@
 
 MEM_PATH=$(cat memory_path.txt)
 CORPUS=$(cat corpus.txt)
-WINDOW_SIZE=1
+WINDOW_SIZE=5
 N_NEGATIVE=25
-N_THREADS=8
-N_ITER=10
+N_THREADS=4
+N_ITER=40
 SAVE_AS_BIN=0
 MIN_COUNT=0
 time ./word2vec -train $MEM_PATH/$CORPUS/$CORPUS.txt -output word2vec_cbow$WINDOW_SIZE.txt -save-vocab vocab_word2vec_cbow$WINDOW_SIZE.txt -min-count $MIN_COUNT -cbow 1 -size 200 -window $WINDOW_SIZE -negative $N_NEGATIVE -hs 0 -sample 0 -threads $N_THREADS -binary $SAVE_AS_BIN -iter $N_ITER
